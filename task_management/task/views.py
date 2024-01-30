983b6a64d6656f3cd224b6398d8ef5f70cd250ae
Jobayer98 <joobayerrahman0220@gmail.com>
2024-01-30 13:21:32 +0600

    add task viewdetails, update and delete

--- task_management/task/views.py
+++ task_management/task/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import SignUpForm, LoginForm, TaskForm
+from .models import Task
 
-# Create your views here.
+class SignUpView(CreateView):
+    template_name = 'task/signup.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('task_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data.get('username')
+        raw_password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=raw_password)
+        login(self.request, user)
+        return response
+
+class LoginView(TemplateView):
+    template_name = 'task/login.html'
+
+    def post(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('task_list')
+        return self.render_to_response({'form': form})
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task/task_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        return Task.objects.all()
+    
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task/task_detail.html'
+    context_object_name = 'task'
+
+class CreateTaskView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'task/create_task.html'
+    success_url = reverse_lazy('task/task_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'task/update_task.html'
+    form_class = TaskForm
+    success_url = reverse_lazy('task_list')
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'task/delete_task.html'
+    success_url = reverse_lazy('task_list')
