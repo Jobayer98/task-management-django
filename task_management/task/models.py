521bb081838b0166607fba00dc6b875731085978
Jobayer98 <joobayerrahman0220@gmail.com>
2024-01-30 11:31:59 +0600

    add task model and taskimage model

--- task_management/task/models.py
+++ task_management/task/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    priority_choices = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    priority = models.CharField(max_length=10, choices=priority_choices)
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+class TaskPhoto(models.Model):
+    task = models.ForeignKey(Task, related_name='photos', on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='task_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.task.title} - Photo {self.id}'
